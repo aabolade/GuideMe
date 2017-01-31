761065b5adc6e1c97f03358da722c27c54c20263
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2017-01-31 17:32:24 +0000

    Adds distance update method

--- GuideMe/GuideMe/ViewController.swift
+++ GuideMe/GuideMe/ViewController.swift
@@ -30,7 +30,47 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    startScanning()
+                }
+            }
+           
+        }
+    }
+    
+    func startScanning() {
+        let uuid = UUID(uuidString: "b9407f30f5f8466eaff925556b57fe6d")
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid!, major: 7222, minor:  65159, identifier: "Beacon")
+        
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func update(distance: CLProximity) {
+        UIView.animate(withDuration: 0.8) { [unowned self ] in
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                self.distanceReading.text = "UNKNOWN"
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                self.distanceReading.text = "FAR"
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                self.distanceReading.text = "Near"
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+                self.distanceReading.text = "RIGHT HERE"
+            }
+        }
+    }
 
 }
 
