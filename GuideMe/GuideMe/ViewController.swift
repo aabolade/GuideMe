cde442e3c731163e74d183040362568907f61e29
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-01-31 17:09:30 +0000

    creating new xcode project with location permission

---
+++ GuideMe/GuideMe/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  GuideMe
+//
+//  Created by Leke Abolade on 31/01/2017.
+//  Copyright © 2017 Leke Abolade. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager: CLLocationManager!
+
+    @IBOutlet weak var distanceReading: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        view.backgroundColor = UIColor.gray
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
