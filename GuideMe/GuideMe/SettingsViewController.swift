c7bbd21376c005e7fe3c7f70c96d310162c94654
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2017-02-02 18:25:22 +0000

    Adds settingViewController

---
+++ GuideMe/GuideMe/SettingsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsViewController.swift
+//  GuideMe
+//
+//  Created by Courtney Osborn on 02/02/2017.
+//  Copyright © 2017 Leke Abolade. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
