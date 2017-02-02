d60dd67d3b4c430ab18fa07c3bf5eb9f26abd580
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-02-02 17:15:15 +0000

    adding a beep before message

--- GuideMe/GuideMe/Speech.swift
+++ GuideMe/GuideMe/Speech.swift
@@ -13,12 +13,29 @@ class Speech {
     
     let synth = AVSpeechSynthesizer()
     var myUtterance = AVSpeechUtterance(string: "Guide me has begun scanning")
+    var player: AVAudioPlayer?
     
     func textToSpeechSettings(string: String) {
         myUtterance = AVSpeechUtterance(string: string)
         myUtterance.rate = 0.3
         myUtterance.volume = 1.0
         synth.speak(myUtterance)
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "beep", withExtension: "wav") else {
+            print("url is nil")
+            return
+        }
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else {return}
+            
+            player.prepareToPlay()
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 
 }
