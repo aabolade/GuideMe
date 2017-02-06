7f8757cf48af37654631dc6045aea7766c489790
Pea M. Tyczynska <CrystalPea@github.com>
2017-02-06 13:47:15 +0000

    first unit test file created

---
+++ GuideMe/GuideMeTests.swift
@@ -0,0 +1,29 @@
+//
+//  GuideMeTests.swift
+//  GuideMe
+//
+//  Created by Pea M. Tyczynska on 06/02/2017.
+//  Copyright © 2017 Leke Abolade. All rights reserved.
+//
+
+import XCTest
+@testable import GuideMe
+
+class GuideMeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
