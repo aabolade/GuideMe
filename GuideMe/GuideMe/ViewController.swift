a2834b36450b8d61ed3ae57924a9f97b7b65dfaa
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2017-02-08 15:13:15 +0000

    Separates direction instructions in new class

--- GuideMe/GuideMe/ViewController.swift
+++ GuideMe/GuideMe/ViewController.swift
@@ -14,7 +14,7 @@ import Speech
 
 class ViewController: UIViewController, CLLocationManagerDelegate{
     
-    
+    var beaconResponse = BeaconResponse()
     var locationManager: CLLocationManager!
     var message: String = ""
 
@@ -89,33 +89,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
  
     }
     
-    var fromRoad: [Int: String] = [
-
-        1: "Stairs ahead, follow the handrail on the left, go down 13 steps",
-        41693: "Follow the handrail Left, 180 degrees, go down 13 steps",
-        49281: "You are at the bottom of the stairs, Keep Right against the Wall, walk straight ahead",
-        50300: "Turn Left in 5 steps",
-        50500: "You are approaching the ticket barriers, ticket machines are on your left, Keep Left for the Wide Gate",
-        50800: "Turn Right, for westbound platform",
-        65159: "You are now on the Westbound Platform"
-    ]
-
-    var fromPlatform: [Int: String] = [
-        
-        65159: "You are now on the Algate platform",
-        60000: "Turn Left for WhiteChapel Road",
-        50800: "Turn Right for Commercial Street",
-        50500: "You are approaching the ticket barriers, Keep Right for the Wide Gate",
-        50300: "Keep Right, follow the wall and turn right",
-        49281: "Stairs Ahead, go up 13 steps, handrail on your left",
-        41693: "Follow the handrail Left, 180 degrees, go up 13 steps",
-        1: "You are now exiting Algate station"
-    ]
-
     
     func enterFromRoad(beaconNumber: Int) {
 
-        guard let unwrappedMessage = fromRoad[beaconNumber] else {
+        guard let unwrappedMessage = beaconResponse.fromRoad[beaconNumber] else {
             print ("I don't recognise this beacon")
             return
         }
@@ -128,7 +105,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
     }
     
     func enterFromTrain(beaconNumber: Int) {
-        guard let unwrappedMessage = fromPlatform[beaconNumber] else {
+        guard let unwrappedMessage = beaconResponse.fromPlatform[beaconNumber] else {
             print ("I don't recognise this beacon")
             return
         }
